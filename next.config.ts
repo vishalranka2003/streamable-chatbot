04f1fdf2780ce377b2e1c3a5c0f305aef4e08bce
Vishal Ranka <vishalranka3@gmail.com>
2024-12-18 01:34:39 +0530

    config updated

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // add enviroment variables here to be used in the app
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
 };
 
 export default nextConfig;
